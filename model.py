ed11906bc7d0d317a81960a9eff791b4e6fd8920
Karandeep Singh Nanda <karandeepsinghnanda@Karandeeps-MacBook-Pro.local>
2023-09-13 10:49:39 -0600

    added PositionalEncoding

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
-import torch.nn
+import torch.nn as nn
+import math
 
 class InputEmbeddings(nn.Module):
     
@@ -8,6 +9,26 @@ class InputEmbeddings(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size,d_model)
+        
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.module):
+    
+    def __init__(self, d_model: int, seq_len:int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        #Create a matrix of shape(seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        
+        # Create a vector of shape(seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term= torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(1000.0)/ d_model))
+        
         
         
     
