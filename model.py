89025c5e4f1d766ab5c9d793c1fea978a7212429
Karandeep Singh Nanda <karandeepsinghnanda@Karandeeps-MacBook-Pro.local>
2023-09-13 10:48:43 -0600

    added PositionalEncoding

--- model.py
+++ model.py
@@ -5,5 +5,9 @@ class InputEmbeddings(nn.Module):
     
     
     def __init__(self,d_model:int, vocab_size :int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        
         
     
