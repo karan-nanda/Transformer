7f12988bb2c91741e9dfa3edca0909be86cca1fb
Karandeep Singh Nanda <karandeepsinghnanda@karandeeps-mbp.lan>
2023-09-13 23:07:05 -0600

    updated model.py

--- model.py
+++ model.py
@@ -224,6 +224,30 @@ class Transformer(nn.Module):
     def project(self, x):
         return self.projection_layer(x)
     
+def build_transformer(src_vocab_size : int, tgt_vocab_size : int, src_seq_len : int, tgt_seq_len : int, d_model : int=512, N : int =6,h :int=8, dropout : float =0.1, d_ff : int =2048) -> Transformer:
+    #Create embedding layers
+    src_embed = InputEmbeddings(d_model, src_vocab_size)
+    tgt_embed = InputEmbeddings(d_model, tgt_vocab_size)
+    
+    #Create the positional encoding layers
+    src_pos = PositionalEncoding(d_model, src_seq_len, dropout)
+    tgt_pos = PositionalEncoding(d_model, tgt_seq_len,dropout)
+    
+    #Create the encoder blocks
+    encoder_blocks = []
+    
+    for _ in range(N):
+        encoder_self_attention_block = MultiHeadAttentionBlock(d_model,h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model,d_ff, dropout)
+        encoder_block = EncoderBlock(d_model, encoder_self_attention_block, feed_forward_block, dropout)
+        encoder_blocks.append(encoder_block)
+        
+    #Create the decoder blocks
+    decoder_blocks = []
+    
+    for _ in range(N):
+        
+        
     
     
             
