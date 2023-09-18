68f9486b3f7e6f0aab74223068530b9dd911d2de
Karandeep Singh Nanda <karandeepsinghnanda@Karandeeps-MacBook-Pro.local>
2023-09-18 09:18:39 -0600

    finished dataset.py

--- dataset.py
+++ dataset.py
@@ -52,3 +52,40 @@ class BilingualDataset(Dataset):
         )
         
         #Add the </s> token
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype = torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype= torch.int64),
+            ],
+            dim = 0,
+        )
+        
+        #Add the </s> token
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype = torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype= torch.int64),
+            ],
+            dim = 0,
+        )
+        
+        #Double check the size of tensors to equal seq_len length
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+        
+        return{
+            "encoder_input": encoder_input, #(seq_len)
+            'decoder_input' : decoder_input, # (seq_len)
+            'encoder_mask' : (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), #(1,1,seq_len)
+            'decoder_mask' : (decoder_input != self.pad_token).unsqueeze(0).int() & causal_mask(decoder_input.size(0)), # (1, seq_len) & (1, seq_len, seq_len)
+            'label' : label,
+            'src_text': src_text,
+            'tgt_text' : tgt_text,   
+        }
+        
+def causal_mask(size):
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+    return mask == 0
