184a332e83fdda0db3d563144c0269e5da11e751
Karandeep Singh Nanda <karandeepsinghnanda@karandeeps-mbp.lan>
2023-09-18 21:33:51 -0600

    updated train_wb.py

--- train_wb.py
+++ train_wb.py
@@ -22,3 +22,22 @@ from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
 import wandb
+
+import torchmetrics
+
+
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+    
+    #Precompute the encoder output and reuse it for every step
+    encoder_output = model.encode(source, source_mask)
+    
+    #Initialize the decoder output with the sos token
+    decoder_input = torch.empty(1,1).fill_(sos_idx).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+        
+        #Build the mask for the target
+        decoder_mask = causal_mask(decoder_input.size(1))
