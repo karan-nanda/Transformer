e93705337f3bcc568a2e6627e586f9c335a8e76b
Karandeep Singh Nanda <karandeepsinghnanda@karandeeps-mbp.lan>
2023-09-19 13:07:00 -0600

    fixed some bugs

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from model import build_transformer
 from dataset import BilingualDataset, causal_mask
-from config import get_config, get_weights_file_path
+from config import get_config, get_weights_file_path, latest_weights_file_path
 
 import torchtext.datasets as datasets
 import torch
@@ -152,7 +152,7 @@ def get_ds(config):
     
     for item in ds_raw:
         src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
-        tgt_ids = tokenizer_tgt(item['translation'][config['lang_tgt']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
         max_len_src = max(max_len_src, len(src_ids))
         max_len_tgt = max(max_len_tgt, len(tgt_ids))
         
@@ -175,7 +175,7 @@ def train_model(config):
     print("Using device", device)
     
     #Make sure the Weight folder exists
-    Path(config['model_folder']).mkdir(parents=True, exist_ok=True)
+    Path(f"{config['datasource']}_{config['model_folder']}").mkdir(parents=True, exist_ok=True)
     
     train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
     model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
@@ -186,14 +186,17 @@ def train_model(config):
     
     initial_epoch = 0
     global_step = 0
-    if config['preload']:
-        model_filename = get_weights_file_path(config,config['preload'])
+    preload = config['preload']
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    if model_filename:
         print(f'Preloading model {model_filename}')
         state = torch.load(model_filename)
-        model.load_state_dict(state['model_dict_state'])
+        model.load_state_dict(state['model_state_dict'])
         initial_epoch = state['epoch'] + 1
         optimizer.load_state_dict(state['optimizer_state_dict'])
         global_step = state['global_step']
+    else:
+        print('No model to preload, starting from scratch')
         
     loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
     
